9a3d044c331ce1a63618dce44f04736ba0a816fa
ShiroRikka <2985772684@qq.com>
2025-06-25 19:52:20 +0800

    feat(proxy): 调整代理测试逻辑并优化重复IP删除时机
    - 将 `delete_duplicate_ips` 方法调用移至主循环内，确保每次迭代时清理重复IP
    - 新增打印信息，区分当前测试的是低分代理还是高分代理
    - 更新数据库文件以适配逻辑调整

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ import convert
 dc.create()
 dc.delete_old_ip()
 dc.multiple_insert(gp.get_proxy())
-dc.delete_duplicate_ips()
 
 dc.create_index()
 tester = ProxyTester()
 
 while True:
-
+    dc.delete_duplicate_ips()
     if dc.count_low_score_proxies() > 0 :
         ip_list = tester.test_proxy_list(dc.get_ips_to_test(16), 16)
+        print(f"当前测试的是低分代理")
     else:
         ip_list = tester.test_proxy_list(dc.get_best_ips_to_test(16), 16)
+        print(f"当前测试的是高分代理")
 
 
     ip = convert.convert_ip_status_list(ip_list)
